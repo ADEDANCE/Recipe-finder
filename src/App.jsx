b7714a3b2e430989338cf2e3d56af4d27a0b5231
AbdganiyuUthmanBonuola <uthmanadedance@gmail.com>
2025-02-02 16:17:22 +0100

    Add new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import RicipePage from './RicipePage'
+import RecipePage from './RecipePage'
+import RecipeDetails from './RecipeDetails'
 import './App.css'
 import Navbar from './Navbar'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -14,7 +15,9 @@ function App() {
       <Navbar />
       <Router >
                 <Routes>
-                           <Route path='/' element={<RicipePage/>} />
+                        
+                           <Route path='/' element={<RecipePage/>} />
+                           <Route path='/RecipeDetails' element={<RecipeDetails />} />
                            {/* <Route path='/Navbar' element={ <Navbar />} /> */}
                 </Routes>
       </Router>

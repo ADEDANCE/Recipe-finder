ed4c81b6fde77a638fc7da578486446440227ddc
AbdganiyuUthmanBonuola <uthmanadedance@gmail.com>
2025-02-02 16:13:11 +0100

    Rename this component

---
+++ src/RecipePage.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import RecipeDetails from "./RecipeDetails";
+import {useNavigate} from 'react-router-dom'
+
+const API_KEY = "aaa825c706f24d959e58185fc7ae9601";
+
+const RecipePage = () => {
+  const [ingredients, setIngredient] = useState("");
+  const [recipes, setRecipes] = useState([]);
+ const navigate =useNavigate();
+
+ const handleViewDetails = () => {
+      navigate('/RecipeDetails');
+ } 
+
+  const fetchRecipes = async () => {
+    if (!ingredients.trim()) return;
+
+    const url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=10&apiKey=${API_KEY}`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setRecipes(data);
+    } catch (error) {
+      console.error("Error fetching recipes:", error);
+    }
+  };
+
+  // const fetchRecipeDetails = async (recipeId) => {
+  //   const url = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${API_KEY}`;
+
+  //   try {
+  //     const response = await fetch(url);
+  //     if (!response.ok) {
+  //       throw new Error(`API request failed with status ${response.status}`);
+  //     }
+  //     const data = await response.json();
+  //     setDetails(data);
+  //   } catch (error) {
+  //     console.error("Error fetching details:", error.message);
+  //   }
+  // };
+
+  return (
+    <div className="Recipepage">
+      <div
+        className="WMessage"
+        style={{
+          backgroundColor: "red",
+        }}
+      >
+        <h2>Recipe search</h2>
+        <p>
+          Our recipe finder contains hundreds of heart-healthy recipes with full
+          nutritional analysis.
+        </p>
+      </div>
+
+      <div className="userinputs">
+        <input
+          type="text"
+          value={ingredients}
+          onChange={(e) => setIngredient(e.target.value)}
+        />
+        <button onClick={fetchRecipes}>Search</button>
+      </div>
+
+      <div
+        style={{
+          backgroundColor: "blueviolet",
+          alignItems: "center",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {recipes.length > 0 && <h2 className="Recipesheading">Matching Recipes:</h2>}
+        <ul>
+          {recipes.map((recipe) => (
+            <li key={recipe.id}>
+              <Row className="container recipes">
+                <Col md={9}>
+                  <h3>{recipe.title}</h3>
+                  <button
+                    onClick={handleViewDetails}
+                    style={{
+                      marginTop: "10px",
+                      padding: "5px 10px",
+                      cursor: "pointer",
+                    }}
+                  >
+                    View Recipe
+                  </button>
+                </Col>
+                <Col md={2}>
+                  <img src={recipe.image} alt="" style={{ width: "200px" }} />
+                </Col>
+              </Row>
+              <br />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* {details && <RecipeDetails recipe={details} />} */}
+    </div>
+  );
+};
+
+export default RecipePage;
